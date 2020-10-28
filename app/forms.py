cb68d659479dfd81e58e64a45bc6fb0fd837fd1b
rishan <rishanrm@gmail.com>
2020-10-27 18:03:09 -0700

    Add validation to contact form

--- app/forms.py
+++ app/forms.py
@@ -2,12 +2,31 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
+# class ContactForm(FlaskForm):
+#     """Contact form."""
+#     name = StringField('Name', validators=[DataRequired()])
+#     email = StringField('Email', validators=[DataRequired(), Email(message=('Not a valid email address.'))])
+#     organization = StringField('Organization (optional)')
+#     message = TextAreaField('Message',
+#                         validators=[DataRequired(), Length(min=4, message=('Your message is too short.'))])
+#     # recaptcha = RecaptchaField()
+#     submit = SubmitField('Submit')
+
+
 class ContactForm(FlaskForm):
     """Contact form."""
+    # name = StringField('Name', validators=[DataRequired()])
+    # email = StringField('Email', validators=[DataRequired(), Email(message=('Not a valid email address.'))])
+    # organization = StringField('Organization (optional)')
+    # message = TextAreaField('Message',
+    #                         validators=[DataRequired(), Length(min=4, message=('Your message is too short.'))])
+    # # recaptcha = RecaptchaField()
+    # submit = SubmitField('Submit')
+
     name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email(message=('Not a valid email address.'))])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     organization = StringField('Organization (optional)')
     message = TextAreaField('Message',
-                        validators=[DataRequired(), Length(min=4, message=('Your message is too short.'))])
+        validators=[DataRequired(), Length(min=4, message=('Your message is too short.'))])
     # recaptcha = RecaptchaField()
     submit = SubmitField('Submit')
