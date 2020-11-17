4fb0444df762c40a0374b4f39909d7cd692c106d
rishanrm <rishanrm@gmail.com>
2020-11-16 18:34:44 -0800

    Move sidebar fix into index.js

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -188,14 +188,59 @@
 //  -->
 
 
-
-    <!-- 
         $('#table').on('pre-body.bs.table', function (e, arg1, arg2) {
             console.log('PRE BODY RAN')
             $('div.hidden').fadeIn(2000).removeClass('hidden');
 
         });
-     -->
+
+
+// Sidebar menu closing fix
+
+var checkbox = document.querySelector("input[id=openSidebarMenu]");
+var div = document.createElement('div');
+div.id = 'left-nav-cover';
+
+function triggerEvent(element, eventName) {
+    if (typeof checkbox != null) {
+        document.addEventListener('click', function (e) {
+            e = e || window.event;
+            var target = e.target || e.srcElement;
+            if ((target.getAttribute("id") != "sidebarMenu" && target.getAttribute("id") != "openSidebarMenu") && document.getElementById("openSidebarMenu").checked == true) {
+                document.getElementById("openSidebarMenu").checked = false;
+                triggerEvent(checkbox, 'change');
+                console.log("CHANGE")
+            };
+            if (target.getAttribute("id") == "openSidebarMenu" && document.getElementById("openSidebarMenu").checked == false) {
+                document.getElementById("openSidebarMenu").checked = false;
+                triggerEvent(checkbox, 'change');
+                console.log("CHANGE")
+            };
+        }, false);
+
+        // function triggerEvent(element, eventName) {
+        //     var manualEvent = document.createEvent("HTMLEvents");
+        //     manualEvent.initEvent(eventName, false, true);
+        //     element.dispatchEvent(manualEvent);
+        // }
+
+        // var div = document.createElement('div');
+        // div.id = 'left-nav-cover';
+        document.getElementsByTagName('body')[0].appendChild(div);
+        // checkbox.addEventListener('change', function () {
+            if (checkbox.checked) {
+                div.innerHTML = "<div id='left-nav-cover-inner'></div>";
+                console.log("CREATED")
+            } else {
+                div.removeChild(document.getElementById("left-nav-cover-inner"));
+                console.log("REMOVED")
+            }
+        // });
+    }
+}
+// End sidebar menu closing fix
+
+
 
     // <!-- NOT CURRENTLY IN USE: -->
 
