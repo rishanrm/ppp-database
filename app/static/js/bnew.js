1f32220d40f9b40d1f625836e6b5445fa7fcad83
rishanrm <rishanrm@gmail.com>
2020-11-21 00:42:20 -0800

    Remove unnecessary code from views on bnew.js

--- app/static/js/bnew.js
+++ app/static/js/bnew.js
@@ -54,14 +54,8 @@ barba.init({
         afterLeave({ next }) {
             var indexJS = $("script[src*='" + 'index.js' + "']")
             if (indexJS) {
-                console.log('found it!')
                 indexJS.remove();
-            } else {
-                let script = document.createElement('script');
-                script.src = '/js/index.js';
-                next.container.appendChild(script);
-                console.log("RAN INIT ON BARBA");
-        }
+            }
         },
 
         afterEnter({ next }) {
