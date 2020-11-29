67b074ec85fa6618d128c1497b80339ca4943732
rishanrm <rishanrm@gmail.com>
2020-11-28 18:17:58 -0800

    Move email composition to separate email class

---
+++ app/email.py
@@ -0,0 +1,63 @@
+import smtplib
+import ssl
+
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+from app.config import Config
+
+class Email():
+
+    @staticmethod
+    def send_email(form):
+        sender_email = Config.CONTACT_EMAIL_ADDR
+        receiver_email = Config.CONTACT_EMAIL_ADDR
+
+        message = MIMEMultipart("alternative")
+        message["Subject"] = f"PPP Data Contact Form Submission: {form.email.data}"
+        message["From"] = sender_email
+        message["To"] = receiver_email
+
+        # Create the plain-text and HTML version of your message
+        text = f"""\
+PPP Data Contact Form Submission
+
+Name: {form.name.data}
+
+Email: {form.email.data}
+
+Organization: {form.organization.data}
+
+Message: {form.message.data}"""
+
+        html = f"""\
+        <html>
+        <body>
+            <h2>PPP Data Contact Form Submission</h2>
+            <p>
+                <b style='color: #037AFB;'>Name:</b> {form.name.data}<br><br>
+                <b style='color: #037AFB;'>Email:</b> {form.email.data}<br><br>
+                <b style='color: #037AFB;'>Organization:</b> {form.organization.data}<br><br>
+                <b style='color: #037AFB;'>Message:</b> {form.message.data}
+            </p>
+        </body>
+        </html>
+"""
+
+        # Turn these into plain/html MIMEText objects
+        part1 = MIMEText(text, "plain")
+        part2 = MIMEText(html, "html")
+
+        # Add HTML/plain-text parts to MIMEMultipart message
+        # The email client will try to render the last part first
+        message.attach(part1)
+        message.attach(part2)
+
+        # Create secure connection with server and send email
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            # with smtplib.SMTP_SSL("smtp.gmail.com", 587, context=context) as server:
+            server.login(Config.CONTACT_EMAIL_ADDR, Config.CONTACT_EMAIL_PASS)
+            server.sendmail(
+                sender_email, receiver_email, message.as_string()
+            )
--- app/main/routes.py
+++ app/main/routes.py
@@ -9,7 +9,8 @@ from app import db
 #from app import app
 from app.config import Config
 from app.database import DatabaseConnection
-import os
+from app.email import Email
+# import os
 import smtplib
 import ssl
 from email.mime.text import MIMEText
@@ -37,92 +38,14 @@ def data_150k_and_up():
 def about():
     return render_template('about.html')
 
-# @main.route("/contact")
-# def contact():
-#     return render_template('contact.html')
-
 @main.route("/contact", methods=["GET", "POST"])
 def contact():
     form = ContactForm()
     if form.validate_on_submit():
-        sender_email = Config.CONTACT_EMAIL_ADDR
-        receiver_email = Config.CONTACT_EMAIL_ADDR
-        # password = input("Type your password and press enter:")
-
-        message = MIMEMultipart("alternative")
-        message["Subject"] = f"PPP Data Contact Form Submission: {form.email.data}"
-        message["From"] = sender_email
-        message["To"] = receiver_email
-
-        # Create the plain-text and HTML version of your message
-        text = f"""\
-PPP Data Contact Form Submission
-
-Name: {form.name.data}
-
-Email: {form.email.data}
-
-Organization: {form.organization.data}
-
-Message: {form.message.data}"""
-
-        html = f"""\
-        <html>
-        <body>
-            <h2>PPP Data Contact Form Submission</h2>
-            <p>
-                <b style='color: #037AFB;'>Name:</b> {form.name.data}<br><br>
-                <b style='color: #037AFB;'>Email:</b> {form.email.data}<br><br>
-                <b style='color: #037AFB;'>Organization:</b> {form.organization.data}<br><br>
-                <b style='color: #037AFB;'>Message:</b> {form.message.data}
-            </p>
-        </body>
-        </html>
-        """
-
-        # Turn these into plain/html MIMEText objects
-        part1 = MIMEText(text, "plain")
-        part2 = MIMEText(html, "html")
-
-        # Add HTML/plain-text parts to MIMEMultipart message
-        # The email client will try to render the last part first
-        message.attach(part1)
-        message.attach(part2)
-
-        # Create secure connection with server and send email
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        # with smtplib.SMTP_SSL("smtp.gmail.com", 587, context=context) as server:
-            server.login(Config.CONTACT_EMAIL_ADDR, Config.CONTACT_EMAIL_PASS)
-            server.sendmail(
-                sender_email, receiver_email, message.as_string()
-            )
-
-
-#         email_message = f"""Message from the PPP Contact Form:
-
-# Name: {form.name.data}
-
-# Email: {email.data}
-
-# Organization: {organization.data}
-
-# Message: {message.data}
-# """
-#     server = smtplib.SMTP("smtp.gmail.com", 587)
-#     server.starttls()
-#     server.login(Config.CONTACT_EMAIL_ADDR, Config.CONTACT_EMAIL_PASS)
-#     # server.sendmail(Config.CONTACT_EMAIL_ADDR, Config.CONTACT_EMAIL_ADDR, email_message)
-#     # server.login('rishanrm@gmail.com', Config.CONTACT_EMAIL_PASS)
-#     server.sendmail(Config.CONTACT_EMAIL_ADDR,
-#                     Config.CONTACT_EMAIL_ADDR, email_message)
+        Email.send_email(form)
         flash("Your message has been sent!", "success")
-        # return redirect(url_for('main.contact'))
         return redirect(url_for('main.index'))
     return render_template('contact.html', title='Contact', form=form)
-
-
-
 
 @main.route("/privacy")
 def privacy():
