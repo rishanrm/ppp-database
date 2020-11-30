cad5c2bd0299c2b1df3781a3cc13e6b42fa8ad30
rishanrm <rishanrm@gmail.com>
2020-11-30 15:23:06 -0800

    Remove comments from forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,30 +1,8 @@
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
-import smtplib
-from app.config import Config
-
-# class ContactForm(FlaskForm):
-#     """Contact form."""
-#     name = StringField('Name', validators=[DataRequired()])
-#     email = StringField('Email', validators=[DataRequired(), Email(message=('Not a valid email address.'))])
-#     organization = StringField('Organization (optional)')
-#     message = TextAreaField('Message',
-#                         validators=[DataRequired(), Length(min=4, message=('Your message is too short.'))])
-#     # recaptcha = RecaptchaField()
-#     submit = SubmitField('Submit')
-
-
 class ContactForm(FlaskForm):
-    """Contact form."""
-    # name = StringField('Name', validators=[DataRequired()])
-    # email = StringField('Email', validators=[DataRequired(), Email(message=('Not a valid email address.'))])
-    # organization = StringField('Organization (optional)')
-    # message = TextAreaField('Message',
-    #                         validators=[DataRequired(), Length(min=4, message=('Your message is too short.'))])
-    # # recaptcha = RecaptchaField()
-    # submit = SubmitField('Submit')
-
+    
     name = StringField('Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     organization = StringField('Organization (optional)')
