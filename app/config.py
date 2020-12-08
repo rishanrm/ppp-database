979496461c0181667c8d96cac12833bb3430794c
rishanrm <rishanrm@gmail.com>
2020-12-07 23:24:02 -0800

    Break out postgres host for dev and prod

--- app/config.py
+++ app/config.py
@@ -20,27 +20,26 @@ class Config:
     NUMERIC_HEADERS = ["loanamount", "jobsretained", "jobsreported"]
     NAMES_TO_CAPITALIZE = ['businessname', 'address', 'city', 'lender']
 
-    """POSTGRES DATABASE CONNECTION"""
-    POSTGRES_HOST = "localhost"
-
     """GOOGLE CLOUD"""
     GCLOUD_CREDENTIALS = "ppp-data-us-service-account.json"
     GCLOUD_PROJECT_ID = "ppp-data-us"
     GCLOUD_INSTANCE_ID = "ppp-data"
-    GCLOUD_REGION = "us-central"
-    GCLOUD_ZONE = "us-central1-a"
-    GCLOUD_MACHINE_TYPE = "db-custom-1-3840"
+    GCLOUD_INSTANCE_REGION = "us-central1"
+    GCLOUD_INSTANCE_ZONE = "us-central1-a"
+    GCLOUD_INSTANCE_MACHINE_TYPE = "db-custom-1-3840"
     GCLOUD_REQUIRE_SIGNED_URL = False
 #   Machine types: https://cloud.google.com/sql/docs/postgres/create-instance
 
 class ProductionConfig(Config):
 
     DEBUG_STATUS = False
+    """POSTGRES DATABASE CONNECTION"""
+    POSTGRES_HOST = f"/cloudsql/{Config.GCLOUD_PROJECT_ID}:{Config.GCLOUD_INSTANCE_REGION}:{Config.GCLOUD_INSTANCE_ID}"
     POSTGRES_USER = CloudSecrets.get_cloud_secret(
         Config.GCLOUD_PROJECT_ID, "POSTGRES_USER")
     POSTGRES_PASSWORD = CloudSecrets.get_cloud_secret(
         Config.GCLOUD_PROJECT_ID, "POSTGRES_PASSWORD")
-    POSTGRES_PORT = 5431
+    POSTGRES_PORT = 5432
 
     """CONTACT FORM"""
     CONTACT_EMAIL_ADDR = CloudSecrets.get_cloud_secret(
@@ -56,12 +55,14 @@ class ProductionConfig(Config):
         Config.GCLOUD_PROJECT_ID, "RECAPTCHA_PUBLIC_KEY")
     RECAPTCHA_PRIVATE_KEY = CloudSecrets.get_cloud_secret(
         Config.GCLOUD_PROJECT_ID, "RECAPTCHA_PRIVATE_KEY")
-
-    #    TESTING = True #Turn on or off ReCAPTCHA
+    TESTING = False  # True to turn off ReCAPTCHA for testing
 
 class DevelopmentConfig(Config):
 
     DEBUG_STATUS = True
+
+    """POSTGRES DATABASE CONNECTION"""
+    POSTGRES_HOST = "localhost"
     POSTGRES_USER = os.environ.get("POSTGRES_USER")
     POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
     POSTGRES_PORT = 5432
@@ -77,6 +78,4 @@ class DevelopmentConfig(Config):
     RECAPTCHA_PRIVATE_KEY = os.environ.get("RECAPTCHA_PRIVATE_KEY")
     # RECAPTCHA_PUBLIC_KEY = "6LdH9_oZAAAAAPr6GNAc6-ylfVEHc0AHzc4UhwwU"
     # RECAPTCHA_PRIVATE_KEY = "6LdH9_oZAAAAAE2iY2dKeUHqiq-4Jb7SFnOK1Tvu"
-
-    
-    # TESTING = True #Turn on or off ReCAPTCHA
+    TESTING = False # True to turn off ReCAPTCHA for testing
