1a8849bb6139c1c91f2281599fe882bee7a0a3de
rishanrm <rishanrm@gmail.com>
2020-12-18 02:32:45 -0800

    Work-in-progress reset button exploration

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -47,6 +47,8 @@ function callTable(i) {
 
     console.log("COUNT REQUEST RESET TO 0")
     function ajaxRequest(params) {
+        console.log(params)
+        console.log("THOSE WERE THE PARAMS")
         if(page.includes('data-150k-and-up')) {
             initialSortColumn = 'loanrange'
         } else if(page.includes('data-under-150k')) {
@@ -254,7 +256,11 @@ function callTable(i) {
                 requestCount = 0
                 initial_state = true
                 $('table').bootstrapTable('clearFilterControl')
-                $('select[class*="bootstrap-table-filter-control-' + initialFilterColumn + '"]').val(initialFilterValue);
+                console.log("YOU JUST HIT RESET, IS THE COUNT ZERO and INITIAL STATE TRUE?")
+                console.log(requestCount)
+                console.log(initial_state)
+                // $table.bootstrapTable('destroy')
+                $table.bootstrapTable('refresh')
             })
         })
     
@@ -279,8 +285,12 @@ function callTable(i) {
     // <!-- Fix for Toggle All issue where table overlaps bottom pagination controls -->
     
         $('#table').on('post-body.bs.table', function (e, arg1, arg2) {
-            console.log('POST BODY RAN')
+            console.log('POST BODY RAN, WHATS THE INITIAL STATE?')
+            console.log(initial_state)
             $table.bootstrapTable('resetView')
+            if (initial_state) {
+                $('select[class*="bootstrap-table-filter-control-' + initialFilterColumn + '"]').val(initialFilterValue);
+            }
             // $('div.hidden').fadeIn(2000).removeClass('hidden');
         });
 
